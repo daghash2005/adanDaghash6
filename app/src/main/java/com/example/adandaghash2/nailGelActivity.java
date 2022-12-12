6352e24f77e3f7171013e5c9695896ae7212952e
daghash2005 <adandaghash@aocol.net>
2022-12-12 08:31:35 +0200

    jj

--- app/src/main/java/com/example/adandaghash2/nailGelActivity.java
+++ app/src/main/java/com/example/adandaghash2/nailGelActivity.java
@@ -9,75 +9,68 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Gallery;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class nailGelActivity extends AppCompatActivity implements View.OnClickListener{
-    private static final int CAMERA_REQUEST = 0 ; // the code for the request from the camera
+public class nailGelActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int CAMERA_REQUEST = 0 ;// the code for the request from the camera
     private ImageView imageView;
-  private Button takePhoto;
-  private Bitmap bitmap; // this var will hold the photo camera
+    private Button takePhotoButton, galleryButton;
+    private Bitmap bitmap;// this var will hold the photo of the camera
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nail_gel);
-        imageView=findViewById(R.id.imageView);
-        imageView.setOnClickListener(this);
-        takePhoto.setOnClickListener(this);
-        takePhoto =findViewById(R.id.takePhotoButton);
-
-
-
+        //create the display objects so they can be programmed
+        imageView = findViewById(R.id.imageView);
+        takePhotoButton= findViewById(R.id.takePhotoButton);
+        takePhotoButton.setOnClickListener(this);
+        galleryButton = findViewById(R.id.GalleryButton);
+        galleryButton.setOnClickListener(this);
     }
 
     @Override
-    @RequiresApi(Api= Build.VERSION_CODES.M)
+    @RequiresApi (api = Build.VERSION_CODES.M)
     public void onClick(View view) {
-        if (view == takePhoto) {
-            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+        if (view== takePhotoButton){
+            //check if the permission for camera ws not granted
+            if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
                 requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST);
-
-                // camera
-                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(i, CAMERA_REQUEST);
-            } else if (view == imageView)
-            // Gallery
-
+            }
+            // camera
+            Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // i use the intent 3shan ash4l alcamera
+            startActivityForResult(i, CAMERA_REQUEST);
+        }else  if (view == galleryButton){
+            //gallery
         }
-    }// close onclick
-
-    // this meyhod grants the permission according
+    }
+    // this method grants the permission according to the request code
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // if the camera requested permission
-        if (requestCode== CAMERA_REQUEST){
-            if (grantResults[0]== PackageManager.PERMISSION_GRANTED) {
+        if(requestCode == CAMERA_REQUEST){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-            } else{
+            }else {
                 Toast.makeText(this, "No Permission Was Granted", Toast.LENGTH_SHORT).show();
             }
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST && requestCode == RESULT_OK) {
-            bitmap=(Bitmap) data.getExtras().get("data");
+        if (requestCode == CAMERA_REQUEST &&requestCode == RESULT_OK){ // hl ana nj7t aswr aza aa 5od alsora b data w23rd alsora 3n trek al bitmap .
+            bitmap = ( Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(bitmap);
-
-
         }
     }
-}
-
-
 }
